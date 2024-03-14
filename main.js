c3622d1eae228832e2ca481d8ab8c8ceafb3d202
Your Name <you@example.com>
2024-03-14 17:43:20 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function isPalindrome(num) {
+    if (num < 0 || (num % 10 === 0 && num !== 0)) {
+        return false;
+    }
+    
+    let reversedNum = 0;
+    let originalNum = num;
+
+    while (num > 0) {
+        reversedNum = (reversedNum * 10) + (num % 10);
+        num = Math.floor(num / 10);
+    }
+    
+    return originalNum === reversedNum;
+}
+
+var result = isPalindrome(12321);
+console.log(result);
